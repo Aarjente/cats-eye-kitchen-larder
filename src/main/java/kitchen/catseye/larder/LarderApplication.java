ad6ff66ea7a807d3971256b996ff476701b0426c
Aaryaman Girish <aarjente@gmail.com>
2021-11-08 15:49:47 -0800

    Initialized project

---
+++ src/main/java/kitchen/catseye/larder/LarderApplication.java
@@ -0,0 +1,13 @@
+package kitchen.catseye.larder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LarderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kitchen/catseye/larder/LarderApplicationTests.java
@@ -0,0 +1,13 @@
+package kitchen.catseye.larder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
